b5e8c2d133e0c2721b77b0125a27f38997139627
Akbar Yahya <akbaryahya_ym@yahoo>
2023-02-23 17:54:59 +0800

    add more lua

--- data/lua/uid.lua
+++
@@ -1,4 +0,0 @@
-uidobj = CS.UnityEngine.GameObject.Find("/BetaWatermarkCanvas(Clone)/Panel/TxtUID"):GetComponent("Text");
-uid = uidobj.text;
-uid = uid:gsub("UID:", "<color=#c119b1>YuukiPS:</color>")
-uidobj.text = "<color=#00dbe5>"..uid.."</color>"
